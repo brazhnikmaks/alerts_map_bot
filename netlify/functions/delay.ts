68b8179ebf161f1653ca8613913a446b0b78e89e
Maks Brazhnik <brazhnik.m@gmail.com>
2023-01-22 00:57:09 +0200

    test delay

--- netlify/functions/delay.ts
+++ netlify/functions/delay.ts
@@ -2,9 +2,15 @@ import { Handler } from "@netlify/functions";
 import bot from "../../servises/telefram-service";
 
 const handler: Handler = async () => {
-	setTimeout(() => {
-		fetch("https://alerts-map-bot-a6c83d.netlify.app/.netlify/functions/send");
-	}, 8000);
+	await new Promise(() =>
+		setTimeout(
+			() =>
+				fetch(
+					"https://alerts-map-bot-a6c83d.netlify.app/.netlify/functions/send",
+				),
+			8000,
+		),
+	);
 
 	return { statusCode: 200 };
 };
--- netlify/functions/send.ts
+++ netlify/functions/send.ts
@@ -2,11 +2,18 @@ import { Handler } from "@netlify/functions";
 import bot from "../../servises/telefram-service";
 
 const handler: Handler = async () => {
-	setTimeout(() => {
-		fetch("https://alerts-map-bot-a6c83d.netlify.app/.netlify/functions/delay");
-	}, 8000);
-
-	await bot.sendMessage(436262107, new Date().toLocaleString());
+	await Promise.all([
+		new Promise(() =>
+			setTimeout(
+				() =>
+					fetch(
+						"https://alerts-map-bot-a6c83d.netlify.app/.netlify/functions/send",
+					),
+				8000,
+			),
+		),
+		bot.sendMessage(436262107, new Date().toLocaleString()),
+	]);
 	return { statusCode: 200 };
 };
 
