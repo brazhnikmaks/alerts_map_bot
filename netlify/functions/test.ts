4930cab96c68e8b1101d5bd6fa1d354a817811b9
Maks Brazhnik <brazhnik.m@gmail.com>
2023-01-23 00:56:38 +0200

    try fix puppeteer error

--- netlify/functions/test.ts
+++ netlify/functions/test.ts
@@ -1,16 +1,9 @@
 import { Handler } from "@netlify/functions";
-const puppeteer = require("puppeteer-core");
-const chromium = require("@sparticuz/chromium");
+import puppeteer from "puppeteer-serverless";
 
 const handler: Handler = async () => {
-	const browser = await puppeteer.launch({
-		args: chromium.args,
-		defaultViewport: chromium.defaultViewport,
-		executablePath: await chromium.executablePath(),
-		headless: chromium.headless,
-		ignoreHTTPSErrors: true,
-	});
-
+	// @ts-ignore
+	const browser = await puppeteer.launch();
 	const page = await browser.newPage();
 	await page.goto("https://alerts.in.ua/");
 	const pageTitle = await page.title();
