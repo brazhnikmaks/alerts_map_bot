eb341d76131caad5687eaad8d1147a66e577c20f
Maks Brazhnik <brazhnik.m@gmail.com>
2023-01-23 00:31:07 +0200

    try vercel

--- api/monitor.ts
+++ api/monitor.ts
@@ -1,19 +1,21 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import botController from "../controllers/bot-controller";
+const puppeteer = require("puppeteer-core");
+const chromium = require("chrome-aws-lambda");
 
 export default async (request: VercelRequest, response: VercelResponse) => {
-	console.log("in");
-	await Promise.all([
-		botController.monitor.bind(botController)(),
-		// new Promise(() =>
-		// 	setTimeout(
-		// 		() =>
-		// 			axios(
-		// 				"https://alerts-map-bot-a6c83d.netlify.app/.netlify/functions/delay",
-		// 			),
-		// 		9500,
-		// 	),
-		// ),
-	]);
+	const browser = await puppeteer.launch({
+		args: chromium.args,
+		defaultViewport: chromium.defaultViewport,
+		executablePath: await chromium.executablePath,
+		headless: chromium.headless,
+		ignoreHTTPSErrors: true,
+	});
+
+	const page = await browser.newPage();
+	await page.goto("https://alerts.in.ua/");
+	const pageTitle = await page.title();
+	await browser.close();
+
+	console.log(pageTitle);
 	response.status(200).send(`200`);
 };
