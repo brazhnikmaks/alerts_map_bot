cce0d58d2c501b054074b7081506f728d22e8034
Maks Brazhnik <brazhnik.m@gmail.com>
2023-01-22 02:58:00 +0200

    try fix puppeteer error

--- servises/puppeteer-service.ts
+++ servises/puppeteer-service.ts
@@ -1,5 +1,6 @@
 import { config } from "dotenv";
-const chromium = require("chrome-aws-lambda");
+const puppeteer = require("puppeteer-core");
+const chromium = require("@sparticuz/chromium");
 
 config();
 
@@ -9,7 +10,7 @@ class PuppeteerService {
 		selector: string,
 		evaluate: () => void,
 	) {
-		const browser = await chromium.puppeteer.launch({
+		const browser = await puppeteer.launch({
 			args: chromium.args,
 			defaultViewport: { width: 1920, height: 1080 },
 			executablePath:
